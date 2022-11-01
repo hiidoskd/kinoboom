fa020bfb71d9994e06597242b39a0151a9895f12
hiidoskd <isaeldar4@gmail.com>
2022-11-01 20:49:41 +0600

    add animation

--- src/components/Dropdown/Dropdown.jsx
+++ src/components/Dropdown/Dropdown.jsx
@@ -2,21 +2,30 @@ import classNames from 'classnames'
 import styles from './Dropdown.module.css'
 import { Link } from 'react-router-dom'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import {
+  faClapperboard,
+  faFilm,
+  faBoxArchive,
+  faLaptop
+} from '@fortawesome/free-solid-svg-icons'
 
 const Dropdown = ({ isOpen }) => {
   const menuItems = [
-    { link: '/movies', name: 'Фильмы', icon: 'icon' },
-    { link: '/series', name: 'Сериалы', icon: 'icon' },
-    { link: '/animated', name: 'Мультфильмы', icon: 'icon' },
-    { link: '/anime', name: 'Сериалы', icon: 'icon' }
+    { link: '/movies', name: 'Фильмы', icon: faClapperboard },
+    { link: '/series', name: 'Сериалы', icon: faFilm },
+    { link: '/animated', name: 'Мультфильмы', icon: faBoxArchive },
+    { link: '/anime', name: 'Аниме', icon: faLaptop }
   ]
 
   return (
     <div className={classNames(styles.dd, isOpen && styles.ddOpen)}>
       <ul>
         {menuItems.map(item => (
-          <li>
-            <Link to={item.link}>{item.name}</Link>
+          <li key={item.name}>
+            <Link to={item.link}>
+              <FontAwesomeIcon icon={item.icon} />
+              {item.name}
+            </Link>
           </li>
         ))}
       </ul>
